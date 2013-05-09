03f239479141654dd398eeb9852a9340e3fe0af2
Calder Phillips-Grafflin <calderpg@fire.(none)>
2013-05-09 13:45:05 -0400

    Re-added the gitignore

--- task_control/task_control.sh
+++
@@ -1,6 +0,0 @@
-roslaunch task_control cart_controllers.launch &
-rosparam set "task_control/testing" true
-rosparam set "task_control/safety" 100.0
-rosparam set "task_control/mode" simulation
-rosparam set "task_control/enforce" true
-rosrun task_control pr2_pose_trajectory_controller.py
